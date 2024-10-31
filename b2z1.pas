b4573843033ec167f84f11816e92c207b202f3cb
Fileger <144237631+Fileger@users.noreply.github.com>
2024-10-31 17:00:59 +0300

    Add files via upload

---
+++ b2z1.pas
@@ -0,0 +1,16 @@
+﻿program zad3;
+var i : integer;
+a: array [1..20] of integer;
+begin
+  for i:=1 to 20 do
+    begin
+    a[i]:=Random(-100,100);
+    writeln(a[i]);
+  if (i>=2) and (a[i-1]>a[i]) then
+    begin
+    writeln('Не упорядочен');
+    exit;
+    end;
+    end;
+  writeln('упорядочен')
+end.
---
+++ b3z4.pas
@@ -0,0 +1,18 @@
+﻿program zad3;
+var i : integer;
+a: array [1..20] of integer;
+begin
+  i:=1;
+  while i<21 do
+    begin
+    a[i]:=Random(-100..100);
+    if a[i]>0 then
+      begin
+      a[i+1]:=a[i];
+      a[i]:=0;
+      i+=2;
+      end
+    else i+=1;
+    end;
+writeln(a)
+end.
